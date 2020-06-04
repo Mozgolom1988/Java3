f1f535a53ac3c2a4d513d7534b23bc441a4c2cd2
Vitaliy Arais <vitaly-88@mail.ru>
2020-06-04 19:06:03 +0600

    доработки дЗ

--- hw7/src/MyTest/MakeTest.java
+++ hw7/src/MyTest/MakeTest.java
@@ -2,6 +2,8 @@ package MyTest;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Comparator;
 
 public class MakeTest {
 
@@ -13,13 +15,12 @@ public class MakeTest {
     //На вход принимает класс
     public void start(Class c) throws RuntimeException{
 
-        Method[] methods = c.getDeclaredMethods();
-
+        ArrayList<Method> methods = new ArrayList<>();
         Method BeforeSuite = null;
         Method AfterSuite = null;
 
         //Проверка методов BeforeSuite и AfterSuite и сохранение их значений
-        for (Method m: methods) {
+        for (Method m: c.getDeclaredMethods()) {
             if (m.isAnnotationPresent(BeforeSuite.class)) {
                 if(BeforeSuite ==null) {
                     BeforeSuite = m;
@@ -35,7 +36,17 @@ public class MakeTest {
                     throw new RuntimeException("В классе имеется больше 1 метода с аннтоацией BeforeSuite");
                 }
             }
+
+            if (m.isAnnotationPresent(Test.class)) {
+                if(m.getAnnotation(Test.class).priority() < 1 || m.getAnnotation(Test.class).priority()>10){
+                    throw new RuntimeException("В классе имеется методы с приоритетом не от 1 до 10");
+                } else {
+                    methods.add(m);
+                }
+            }
         }
+
+        methods.sort((o1, o2) -> o1.getAnnotation(Test.class).priority() - o2.getAnnotation(Test.class).priority());
 
         //Запуск теста:
         if(BeforeSuite !=null) {
@@ -48,16 +59,14 @@ public class MakeTest {
             }
         }
 
-        for (Method m: methods) {
-            if (m.isAnnotationPresent(Test.class)) {
+        for (Method testm: methods) {
                 try {
-                    m.invoke(null);
+                    testm.invoke(null);
                 } catch (IllegalAccessException e) {
                     e.printStackTrace();
                 } catch (InvocationTargetException e) {
                     e.printStackTrace();
                 }
-            }
         }
 
         if(AfterSuite !=null) {
--- hw7/src/com/company/Main.java
+++ hw7/src/com/company/Main.java
@@ -7,7 +7,5 @@ public class Main {
     public static void main(String[] args) {
         MakeTest a_test = new MakeTest();
         a_test.start(TestClass.class);
-
-        System.out.println("Test done");
     }
 }
