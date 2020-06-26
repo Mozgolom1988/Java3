fb32ad3930c31701f1299224e865e3a4530aa16b
Vitaliy Arais <vitaly-88@mail.ru>
2020-06-26 17:17:18 +0600

    Доработки

--- hw7/src/com/company/Main.java
+++ hw7/src/com/company/Main.java
@@ -5,7 +5,24 @@ import MyTest.MakeTest;
 public class Main {
 
     public static void main(String[] args) {
-        MakeTest a_test = new MakeTest();
-        a_test.start(TestClass.class);
+       int s =0;
+       int t = 0;
+       int[] a = {8,4,3,0,7,2,1,5,9,6};
+
+        for (int j = 0; j < 9; j++) {
+            if(a[j] <a[j+1]) {
+                s++;
+                t=a[j];
+                a[j]=a[j+1];
+                a[j+1]=t;
+                }
+
+        }
+
+        System.out.println(s);
+
+
+        // MakeTest a_test = new MakeTest();
+       // a_test.start(TestClass.class);
     }
 }
