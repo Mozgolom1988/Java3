208e2bc4a682ce53f0da2e5371e3eb498416e2ee
Vitaliy Arais <vitaly-88@mail.ru>
2020-07-10 18:23:26 +0600

    Начало работы

--- alg_hw5/src/com/company/Main.java
+++ alg_hw5/src/com/company/Main.java
@@ -3,6 +3,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        
+
+        //h Hanoi
+        decideHanoi(int 3);
+
     }
+
+    private static void decideHanoi(int totalNumberOfCircle) {
+
+    }
+
+
 }
