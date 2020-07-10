6cd4c219e190a97fc503b2210f8b687bd507fb62
Vitaliy Arais <vitaly-88@mail.ru>
2020-07-10 19:14:45 +0600

    Разработка задача1 (алгоритм не успел придумать)

--- alg_hw5/src/com/company/Main.java
+++ alg_hw5/src/com/company/Main.java
@@ -21,33 +21,44 @@ public class Main {
             cell_1[i] = i + 1;
         }
 
-        playHanoi(totalNumberOfCircle, cell_1,cell_2,cell_3)
+        //playHanoi(totalNumberOfCircle, cell_1,cell_2,cell_3)
 
-        /*
         for (int i = 0; i <cell_1.length ; i++) {
             System.out.println(cell_1[i]);
         }
-         */
 
     }
 
     private static void playHanoi(int totalNumberOfCircle, int[] cell_1, int[] cell_2, int[] cell_3) {
         boolean MakeStep = false;
+        int from  = 0;
+        int to =0;
 
         //проверка условия выхода из цикла(в последнем массиве лежат данные в нужном порядке):
         while (!testwin(cell_3)) {
 
-            //if(cell_1[0] != 0) {
-            //}
-
-
-
-
-
+            for (int i = 0; i < cell_3.length; i++) {
+                
+            }
 
         }
 
 
+    }
+
+    private static void makeStep(int[] cell_from, int[] cell_to) {
+        for (int i = 0; i < cell_from.length; i++) {
+            if (cell_from[i] !=0) {
+                for (int j = cell_to.length; j > 0 ; j--) {
+                   if(cell_to[j]==0){
+                       cell_to[j] = cell_from[i];
+                       break;
+                   }
+                }
+                cell_from[i] =0;
+                break;
+            }
+        }
     }
 
     private static boolean testwin(int[] cell_3) {
